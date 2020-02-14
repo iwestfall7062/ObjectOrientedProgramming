2fe9ae5bb0ee2b8604cb649b7fe3cd2acf795958
iwestfall7062 <iwestfall7062@gmail.com>
2020-02-13 18:02:01 -0600

    added new Homework03 exercises

--- 2670/Homework03/Exercise03/src/Exercise03.java
+++ 2670/Homework03/Exercise03/src/Exercise03.java
@@ -90,5 +90,4 @@ public class Exercise03 {
 
     }
 
-    private static String
 }
--- 2670/Homework03/Exercise04/src/Exercise04.java
+++ 2670/Homework03/Exercise04/src/Exercise04.java
@@ -12,9 +12,42 @@
 //all of the data files. Do not assume what years of data will be given (i.e. I may test on a subset of the data or use
 //my own test files).
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Exercise04 {
+
+    //constants
+    public static String INSTRUCTIONS = "This program displays the changing popularity of an entered baby name and\n" +
+            "gender. The program will also display the years with highest and lowest\npopularity. When prompted" +
+            " please enter the name and gender you would like to\nsee data for.\n";
+    public static Scanner KEYBOARD_INPUT = new Scanner(System.in);
 
     public static void main(String[] arguments) {
 
+        System.out.println(INSTRUCTIONS);
+
+        System.out.print("Please enter a name (ex. Emma): ");
+        String nameToSearch = KEYBOARD_INPUT.nextLine();
+        String genderToSearch = getGender();
+
+        File namesFile = new File("yob1880.txt");
+
+    }
+
+    public static String getGender() {
+        String genderToSearch;
+        do {
+            System.out.print("Please enter M or F for gender: ");
+            genderToSearch = KEYBOARD_INPUT.nextLine();
+
+        } while (genderToSearch.equals('M') || genderToSearch.equals('F'));
+        return genderToSearch;
+    }
+
+    public static void processNamesFiles(){
+        for (int counter = 1880; counter <= 2018; counter++) {
+            File namesFile = new File("yob" + counter + ".txt");
+        }
     }
 }
